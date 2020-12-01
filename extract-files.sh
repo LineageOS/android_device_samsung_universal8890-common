59cadefe4538c0ecd8220d23936f38fa9e3c9187
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-12-01 21:39:56 +0100

    universal8890: kang RIL blobs from d2s
     * needed to fix incall audio
    
     * double check if libprotobuf-cpp-full generates
       libprotobuf-cpp-full-3.9.1.so
    
    Change-Id: I6462faae62ed18c512306d0068a18528ad8614d8

--- extract-files.sh
+++ extract-files.sh
@@ -76,12 +76,12 @@ sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib64/libsec-ril.so
 # Replace protobuf with vndk29 compat libs for specified libs
 patchelf --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/libwvhidl.so
 patchelf --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/mediadrm/libwvdrmengine.so
-patchelf --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-full-v29.so $BLOB_ROOT/vendor/lib/libsec-ril-dsds.so
-patchelf --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-full-v29.so $BLOB_ROOT/vendor/lib/libsec-ril.so
-patchelf --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-full-v29.so $BLOB_ROOT/vendor/lib64/libsec-ril-dsds.so
-patchelf --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-full-v29.so $BLOB_ROOT/vendor/lib64/libsec-ril.so
 
 # Remove wpa_supplicant service from wifi.rc
 sed -i "41,51d" $BLOB_ROOT/vendor/etc/init/wifi_sec.rc
+
+# Replace libvndsecril-client with libsecril-client
+patchelf --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib/libwrappergps.so
+patchelf --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib64/libwrappergps.so
 
 "$MY_DIR"/setup-makefiles.sh
