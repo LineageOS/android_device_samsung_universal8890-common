886840b0a9a0a2cdfbab7188304eadb429c4d2d9
ivanmeler <i_ivan@windowslive.com>
2021-04-29 10:07:20 +0200

    [TEMP] universal8890: move vulkan hal out of hw/ folder
        * fixes vulkan on R
    
    Change-Id: Id50b48f9247feee15b307156b871478cd6e359fe

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -56,14 +56,14 @@ EGL_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/,\$(EGL_LIBS))
 
 VULKAN_LIBS := vulkan.exynos5.so
 
-VULKAN_32_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/hw/,\$(VULKAN_LIBS))
+VULKAN_32_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/,\$(VULKAN_LIBS))
 \$(VULKAN_32_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
 	@echo "Copy: Vulkan 32-bit lib: \$@"
 	@mkdir -p \$(dir \$@)
 	@rm -rf \$@
 	\$(hide) cp \$(TARGET_OUT_VENDOR)/lib/egl/libGLES_mali.so \$@
 
-VULKAN_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/hw/,\$(VULKAN_LIBS))
+VULKAN_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/,\$(VULKAN_LIBS))
 \$(VULKAN_64_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
 	@echo "Copy: Vulkan 64-bit lib: \$@"
 	@mkdir -p \$(dir \$@)
